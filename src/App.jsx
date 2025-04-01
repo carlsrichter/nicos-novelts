db1c410c689a39b207d9ad53f6c9de89454b6537
carlsrichter <carlsrichter@gmail.com>
2025-04-01 06:29:31 -0400

    Fix routing issue for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link, useParams } from "react-router-dom";
+import { Routes, Route, Link, useParams } from "react-router-dom";
 
 const Home = () => {
   const designs = ["design1", "design2", "design3", "design4"];
@@ -32,12 +32,10 @@ const DesignPage = () => {
 
 const App = () => {
   return (
-	<BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/:designName" element={<DesignPage />} />
     </Routes>
-	</BrowserRouter>
   );
 };
 
