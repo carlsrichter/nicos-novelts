1d69a71273665b2d5d14d0396bdbc7b7d5dc1eae
carlsrichter <carlsrichter@gmail.com>
2025-04-01 09:36:06 -0400

    Fix routing issues for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nicos-novelts/", // Correct for GitHub Pages deployment
+  base: "/",  // Change this to "/" instead of "/nicos-novelts/"
 });
