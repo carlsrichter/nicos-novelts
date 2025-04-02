66e0683d523da7282930e1fddc19ed60a11b3d75
carlsrichter <carlsrichter@gmail.com>
2025-04-02 07:04:40 -0400

    Fix routing issues for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
 import React, { useEffect } from "react";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
-
-const Home = () => {
-  const designs = [
-		      "you-are-not-alone", 
-		      "you-are-not-alone-mirror", 
-		      "design3", 
-		      "design4"
-			              ];
-
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Available Designs</h1>
-      <ul>
-        {designs.map((design) => (
-          <li key={design}>
-            <Link to={`/${design}`}>{design}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 const RedirectToExternal = ({ url }) => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    window.location.href = url; // Redirect to the external site
+    window.location.href = url; // Redirects to the external site
   }, [url]);
 
   return <p>Redirecting...</p>; // Optional message while redirecting
@@ -36,20 +12,37 @@ const RedirectToExternal = ({ url }) => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route 
-          path="/you-are-not-alone" 
-		      element={<RedirectToExternal url="http://redbubble.com/shop/ap/169662999" />} />
-      <Route 
-          path="/you-are-not-alone-mirror" 
-		      element={<RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />} />
-      <Route 
-          path="/design3" 
-		      element={<RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />} />
-      <Route 
-          path="/design4" 
-		      element={<RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />} />
+      {/* Redirect Home ("/") to Redbubble */}
+      <Route
+        path="/"
+        element={
+          <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
+        }
+      />
 
+      {/* Other design redirects */}
+      <Route
+        path="/you-are-not-alone"
+        element={<RedirectToExternal url="http://redbubble.com/shop/ap/169662999" />}
+      />
+      <Route
+        path="/you-are-not-alone-mirror"
+        element={
+          <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
+        }
+      />
+      <Route
+        path="/design3"
+        element={
+          <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
+        }
+      />
+      <Route
+        path="/design4"
+        element={
+          <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
+        }
+      />
     </Routes>
   );
 };
