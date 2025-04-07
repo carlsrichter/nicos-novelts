6f0f8226564e520712818610eca26150284f05bf
carlsrichter <carlsrichter@gmail.com>
2025-04-06 20:19:26 -0400

    Fix routing issues for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const Links = () => {
   const designs = [
     "you-are-not-alone",
     "you-are-not-alone-mirror",
-    "design3",
-    "design4"
+    "god-grant-me-the-serenity",
+    "god-grant-me-the-serenity-mirror-t"
   ];
 
   return (
@@ -54,17 +54,17 @@ const App = () => {
       <Route
         path="/you-are-not-alone-mirror"
         element={
-          <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
+          <RedirectToExternal url="https://www.redbubble.com/shop/ap/169774936" />
         }
       />
       <Route
-        path="/design3"
+        path="/god-grant-me-the-serenity"
         element={
           <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
         }
       />
       <Route
-        path="/design4"
+        path="/god-grant-me-the-serenity-mirror-t"
         element={
           <RedirectToExternal url="https://www.redbubble.com/people/nicos-novelts/shop?artistUserName=Nicos-NovelTs&asc=u&iaCode=all-departments&sortOrder=top%20selling" />
         }
